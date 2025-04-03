02d067ccc91663735e603d0a7d90b5ed3bb745ce
RivinduTharaka <rivindu.20220728@iit.ac.lk>
2025-04-03 12:20:45 +0530

    cmt

---
+++ src/components/Global/Explore/PressAndMedia/PressAndMedia1.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function PressAndMedia1() {
+  return (
+    <div>PressAndMedia1</div>
+  )
+}
+
+export default PressAndMedia1
---
+++ src/components/Global/Explore/PressAndMedia/PressAndMedia2.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function PressAndMedia2() {
+  return (
+    <div>PressAndMedia2</div>
+  )
+}
+
+export default PressAndMedia2
