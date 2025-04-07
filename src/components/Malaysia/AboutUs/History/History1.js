0afe45ddb4db040692703427bb85713995a53674
RivinduTharaka <rivindu.20220728@iit.ac.lk>
2025-04-07 13:19:56 +0530

    cmt

--- src/components/Malaysia/AboutUs/History/History1.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function History1() {
-  return (
-    <div>History1</div>
-  )
-}
-
-export default History1
--- src/components/Nepal/AboutUs/History/History2.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function History2() {
-  return (
-    <div>History2</div>
-  )
-}
-
-export default History2
